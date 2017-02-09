a4e645aede37cc37484e2ba07cb2d8729a5205e1
Yosimar Valega <yosimar.valega@techandsolve.com>
2017-02-08 20:54:50 -0500

    Refactor Multiplo

--- src/main/java/co/com/techandsolve/codekata/Multiplo.java
+++ src/main/java/co/com/techandsolve/codekata/Multiplo.java
@@ -1,25 +1,19 @@
 package co.com.techandsolve.codekata;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.stream.IntStream;
 
 public class Multiplo {
-	
-	
-	public int sumaMultipoTresCinco(List<Integer> listaNumeros){
-		return listaNumeros.stream().filter(i->(i % 3==0)||(i % 5 ==0)).mapToInt(i->i.intValue()).sum();
-		
-	}
-	
-	public static void main(String[] args) {
-		List<Integer> listaNumeros=new ArrayList<>();
-		for(int i=1; i<1000; i++)
-			listaNumeros.add(i);
-		Multiplo multiplo=new Multiplo();
-		
-		int suma=multiplo.sumaMultipoTresCinco(listaNumeros);
-		 System.out.println(suma);
-		
+
+	public int sumaMultipoTresCinco() {
+		return IntStream.range(1,1000).filter(i -> (i % 3 == 0) || (i % 5 == 0)).sum();
 	}
 
+	public static void main(String[] args) {
+
+		Multiplo multiplo = new Multiplo();
+
+		int suma = multiplo.sumaMultipoTresCinco();
+		System.out.println(suma);
+
+	}
 }
--- src/test/java/co/com/techandsolve/codekata/MultiploTresCincoTest.java
+++ src/test/java/co/com/techandsolve/codekata/MultiploTresCincoTest.java
@@ -1,31 +1,16 @@
 package co.com.techandsolve.codekata;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Assert;
 import org.junit.Test;
 
-
-
 public class MultiploTresCincoTest {
-	
+
 	@Test
-	public void debeSumarMultiploDeTresYCinco(){
-		
-		//arrange
-		Multiplo multiplo=new Multiplo();
-		List<Integer> listaNumeros=new ArrayList<Integer>();
-		
-		//act
-		for(int i=1; i<1000; i++)
-			listaNumeros.add(i);
-		int sumaMultiplo=multiplo.sumaMultipoTresCinco(listaNumeros);
-		
-		
-		//assert
+	public void debeSumarMultiploDeTresYCinco() {
+
+		Multiplo multiplo = new Multiplo();
+		int sumaMultiplo = multiplo.sumaMultipoTresCinco();
 		Assert.assertEquals(233168, sumaMultiplo);
-		
 	}
 
 }
