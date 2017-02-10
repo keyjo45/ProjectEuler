35a7a77d77db4b48caa1a85483b8a9df8762394e
Yosimar Valega <yosimar.valega@techandsolve.com>
2017-02-10 14:21:30 -0500

    Refactor

--- src/main/java/co/com/techandsolve/codekata/Multiplo.java
+++ src/main/java/co/com/techandsolve/codekata/Multiplo.java
@@ -4,15 +4,15 @@ import java.util.stream.IntStream;
 
 public class Multiplo {
 
-	public int sumaMultipoTresCinco() {
-		return IntStream.range(1,1000).filter(i -> (i % 3 == 0) || (i % 5 == 0)).sum();
+	public int sumaMultipoTresCinco(int rango) {
+		return IntStream.range(1,rango).filter(i -> (i % 3 == 0) || (i % 5 == 0)).sum();
 	}
 
 	public static void main(String[] args) {
 
 		Multiplo multiplo = new Multiplo();
 
-		int suma = multiplo.sumaMultipoTresCinco();
+		int suma = multiplo.sumaMultipoTresCinco(1000);
 		System.out.println(suma);
 
 	}
--- src/test/java/co/com/techandsolve/codekata/MultiploTresCincoTest.java
+++ src/test/java/co/com/techandsolve/codekata/MultiploTresCincoTest.java
@@ -4,12 +4,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MultiploTresCincoTest {
+	
+	@Test
+	public void debeSumarMultiploDeTresYCincoPorDebajoDiez() {
+
+		Multiplo multiplo = new Multiplo();
+		int sumaMultiplo = multiplo.sumaMultipoTresCinco(10);
+		Assert.assertEquals(23, sumaMultiplo);
+	}
 
 	@Test
 	public void debeSumarMultiploDeTresYCinco() {
 
 		Multiplo multiplo = new Multiplo();
-		int sumaMultiplo = multiplo.sumaMultipoTresCinco();
+		int sumaMultiplo = multiplo.sumaMultipoTresCinco(1000);
 		Assert.assertEquals(233168, sumaMultiplo);
 	}
 
