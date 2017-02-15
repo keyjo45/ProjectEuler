621182f5d0c8af056b98ad8420272c8f2343dc1d
Yosimar Valega <yosimar.valega@techandsolve.com>
2017-02-15 11:17:02 -0500

    Ejercicio N° 8

---
+++ src/main/java/co/com/techandsolve/codekata/ProductoLargo.java
@@ -0,0 +1,78 @@
+package co.com.techandsolve.codekata;
+
+public class ProductoLargo {
+
+	public long[] convertirNumeroAArreglo(String numero) {
+
+		long[] dato = new long[numero.length()];
+		for (int i = 0; i < numero.length(); i++)
+			dato[i] = numero.charAt(i) - '0';
+		return dato;
+	}
+
+	public long buscarProductoLargo(long[] datos, int rango) {
+		int i = 0;
+		int indiceDatos;
+		long producto = 0;
+		long resultado;
+		long contador = datos.length;
+		while (i < datos.length) {
+			long[] arreglo = new long[rango];
+			indiceDatos = i;
+			int indice = 0;
+			if (contador >= rango) {
+				while (indice < arreglo.length) {
+					arreglo[indice] = datos[indiceDatos];
+					indiceDatos++;
+					indice++;
+				}
+				resultado = productoArreglo(arreglo);
+				if (resultado > producto)
+					producto = resultado;
+			}
+			contador--;
+			i++;
+		}
+
+		return producto;
+	}
+
+	public long productoArreglo(long[] dato) {
+		long producto = 1;
+		for (int i = 0; i < dato.length; i++) {
+			producto *= dato[i];
+		}
+		return producto;
+	}
+
+	public static void main(String[] args) {
+
+		String numero = "73167176531330624919225119674426574742355349194934"
+				+ "96983520312774506326239578318016984801869478851843"
+				+ "85861560789112949495459501737958331952853208805511"
+				+ "12540698747158523863050715693290963295227443043557"
+				+ "66896648950445244523161731856403098711121722383113"
+				+ "62229893423380308135336276614282806444486645238749"
+				+ "30358907296290491560440772390713810515859307960866"
+				+ "70172427121883998797908792274921901699720888093776"
+				+ "65727333001053367881220235421809751254540594752243"
+				+ "52584907711670556013604839586446706324415722155397"
+				+ "53697817977846174064955149290862569321978468622482"
+				+ "83972241375657056057490261407972968652414535100474"
+				+ "82166370484403199890008895243450658541227588666881"
+				+ "16427171479924442928230863465674813919123162824586"
+				+ "17866458359124566529476545682848912883142607690042"
+				+ "24219022671055626321111109370544217506941658960408"
+				+ "07198403850962455444362981230987879927244284909188"
+				+ "84580156166097919133875499200524063689912560717606"
+				+ "05886116467109405077541002256983155200055935729725"
+				+ "71636269561882670428252483600823257530420752963450";
+
+		int rango = 13;
+		ProductoLargo producto = new ProductoLargo();
+
+		long resultado = producto.buscarProductoLargo(producto.convertirNumeroAArreglo(numero), rango);
+
+		System.out.println("El producto mayor es: " + resultado);
+	}
+}
